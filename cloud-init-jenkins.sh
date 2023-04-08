704bf812340fd119d6fb6d7e527a213cc974f4ff
Michael Litsos <34893575+MichaelDee98@users.noreply.github.com>
2023-04-08 12:33:07 +0300

    txt to sh

---
+++ cloud-init-jenkins.sh
@@ -0,0 +1,4 @@
+wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update && sudo apt-get install jenkins -y
+service jenkins restart
