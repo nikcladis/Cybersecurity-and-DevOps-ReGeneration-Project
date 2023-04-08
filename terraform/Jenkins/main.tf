a62b519474858c66d156f2a9ffeb58c3873ee319
Nikolaos Kladis <nikcladis@gmail.com>
2023-04-08 14:19:14 +0300

    added port 8080

--- terraform/Jenkins/main.tf
+++ terraform/Jenkins/main.tf
@@ -48,6 +48,19 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Jenkins"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 # Create network interface
